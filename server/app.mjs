9e8f9ce9fb344cd67d0b9913504edb3bbb3416a9
Krittayapornn <krittayaporn.ww@gmail.com>
2024-06-25 13:22:28 +0700

    feat: add API delete

--- server/app.mjs
+++ server/app.mjs
@@ -1,11 +1,34 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
+app.use(express.json())
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
+
+app.delete("/assignments/:assignmentId", async(req, res)=>{
+  const assignmentIdFromClient = req.params.assignmentId;
+
+  try
+  {
+    await connectionPool.query(
+    `delete from assignments
+     where assignment_id = $1`,
+     [assignmentIdFromClient]
+  );
+}catch {
+  return res.status(500).json({
+    message: "Server could not delete assignment because database connection"
+  });
+}
+
+  return res.status(200).json({
+    message: "Deleted assignment sucessfully"
+  });
+})
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
